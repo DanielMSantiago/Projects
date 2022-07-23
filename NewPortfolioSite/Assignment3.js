b180d33db76d106766ba7491ee0f2e2119fb2db8
Daniel Santiago <danielsantiago@pop-os.localdomain>
2022-07-22 23:36:29 -0400

    Completed Form design and implementation on HTML. Added JS to display values on table but having a error for fNameEntry being undefined

---
+++ NewPortfolioSite/Assignment3.js
@@ -0,0 +1,14 @@
+//Global variables used to store values entered in the form and use across validateForm and postEntries form.
+var fNameEntry = document.forms['myForm']['fullName'].value;
+
+
+
+
+//Function to validate form entries based on certain conditions for each entry
+
+    
+
+
+//Function that posts entries to table created after validation is a success
+
+
