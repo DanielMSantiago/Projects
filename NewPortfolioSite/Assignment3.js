71db7b97dd2e3d76c198812ee62dab4dd6de8270
B31rut <dn.snt0325@gmail.com>
2022-07-27 20:37:59 -0400

    fixed issue with getting entries of form to store in variables. Created variables and store entries for use in validation

--- NewPortfolioSite/Assignment3.js
+++ NewPortfolioSite/Assignment3.js
@@ -1,14 +1,18 @@
 //Global variables used to store values entered in the form and use across validateForm and postEntries form.
-var fNameEntry = document.forms['myForm']['fullName'].value;
-
-
-
+let fullNameEntry = document.forms['myForm']['fullName'].value;
+let addressEntry = document.forms['myForm']['address'].value;
+let cityEntry = document.forms['myForm']['city'].value;
+let stateEntry = document.forms['myForm']['city'].value;
+let zipEntry = document.forms['myForm']['zip'].value;
+let emailEntry = document.forms['myForm']['email'].value;
+let bDateEntry = document.forms['myForm']['bDate'].value;
+let messEntry = document.forms['myForm']['message'].value;
 
 //Function to validate form entries based on certain conditions for each entry
+let validateForm = () => {
+    for (let i = 0; i < fullNameEntry)
+}
 
-    
 
 
 //Function that posts entries to table created after validation is a success
-
-
