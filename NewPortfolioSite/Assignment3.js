1e2611ec148fe5cf45629df214a82ce4be6527f2
Daniel Santiago <danielsantiago@pop-os.localdomain>
2022-07-31 10:31:02 -0400

    Fixed HTML onsubmit to allow alert messages for error to pop up on the browser

--- NewPortfolioSite/Assignment3.js
+++ NewPortfolioSite/Assignment3.js
@@ -1,18 +1,45 @@
-//Global variables used to store values entered in the form and use across validateForm and postEntries form.
-let fullNameEntry = document.forms['myForm']['fullName'].value;
-let addressEntry = document.forms['myForm']['address'].value;
-let cityEntry = document.forms['myForm']['city'].value;
-let stateEntry = document.forms['myForm']['city'].value;
-let zipEntry = document.forms['myForm']['zip'].value;
-let emailEntry = document.forms['myForm']['email'].value;
-let bDateEntry = document.forms['myForm']['bDate'].value;
-let messEntry = document.forms['myForm']['message'].value;
 
-//Function to validate form entries based on certain conditions for each entry
+//Function to validate form entries based on certain conditions for each entry. 
 let validateForm = () => {
-    for (let i = 0; i < fullNameEntry)
+    if(!validateBlank(document.forms['myForm']['fullName'].value, 'Full Name')) return false;
+    if(!validateBlank(document.forms['myForm']['address'].value, 'Address')) return false; 
+    if(!validateBlank(document.forms['myForm']['city'].value, 'City')) return false; 
+    if(!validateBlank(document.forms['myForm']['state'].value, 'State')) return false;
+    if(!validateBlank(document.forms['myForm']['zip'].value, 'Zipcode')) return false; 
+    if(!validateBlank(document.forms['myForm']['email'].value, 'Email')) return false;
+    if(!validateBlank(document.forms['myForm']['bDate'].value, 'Birthday')) return false;
+    if(!validateBlank(document.forms['myForm']['message'].value, 'Message')) return false; 
+
+    if(!validateFullName(document.forms['myForm']['fullName'].value)) return false;
+
+
+    }
+//Function to check if entered value is blank
+let validateBlank = (val, valName) => {
+    if (val == null || val == ''){
+        alert('Please enter a value for this field: ' + valName)
+        return false;
+    }
+    else{
+        return true;
+    }
 }
+//Function to check if first AND last name are present in the field
+let validateFullName = (val) => {
+    let regExpName = /^[a-zA-Z]+ [a-zA-Z]+$/;
+    if (!regExpName.test(val)){
+        alert('Please enter your full name (First and Last)');
+        return false; 
+    }
+    else {
+        return true; 
+    }
+}
+    
 
+//Function to post entries after validation to table created
+/*let postEntries = () => {
+    alert('Full Name: ' + fNameEntry);
+    return true;
+}*/
 
-
-//Function that posts entries to table created after validation is a success
