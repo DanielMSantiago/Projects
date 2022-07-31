2c0c0a61bb4988cafff8023e9727720e2a7ac2ee
Daniel Santiago <danielsantiago@pop-os.localdomain>
2022-07-31 12:51:51 -0400

    Added more validation functions for the form.

--- NewPortfolioSite/Assignment3.js
+++ NewPortfolioSite/Assignment3.js
@@ -1,6 +1,43 @@
 
 //Function to validate form entries based on certain conditions for each entry. 
 let validateForm = () => {
+<<<<<<< HEAD
+=======
+    if(!validateBlank(document.forms['myForm']['fullName'].value, 'Full Name')) return false;
+    if(!validateBlank(document.forms['myForm']['address'].value, 'Address')) return false; 
+    if(!validateBlank(document.forms['myForm']['city'].value, 'City')) return false; 
+    if(!validateBlank(document.forms['myForm']['state'].value, 'State')) return false;
+    if(!validateBlank(document.forms['myForm']['zip'].value, 'Zipcode')) return false; 
+    if(!validateBlank(document.forms['myForm']['email'].value, 'Email')) return false;
+    if(!validateBlank(document.forms['myForm']['bDate'].value, 'Birthday')) return false;
+    if(!validateBlank(document.forms['myForm']['message'].value, 'Message')) return false; 
+
+    if(!validateFullName(document.forms['myForm']['fullName'].value)) return false;
+
+
+    }
+//Function to check if entered value is blank
+let validateBlank = (val, valName) => {
+    if (val == null || val == ''){
+        alert('Please enter a value for this field: ' + valName)
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+//Function to check if first AND last name are present in the field
+let validateFullName = (val) => {
+    let regExpName = /^[a-zA-Z]+ [a-zA-Z]+$/;
+    if (!regExpName.test(val)){
+        alert('Please enter your full name (First and Last)');
+        return false; 
+    }
+    else {
+        return true; 
+    }
+}
+>>>>>>> master
     
     //validate name
     if(!ValidateBlank(document.forms['myForm']['fullName'].value, 'Full Name')) return false;
@@ -29,6 +66,7 @@ let validateForm = () => {
     //validate message
     if(!ValidateBlank(document.forms['myForm']['message'].value, 'Message')) return false; 
 
+<<<<<<< HEAD
 
     }
 //Function to check if entered value is blank
@@ -93,4 +131,11 @@ let postEntries = () => {
     return true;
 }
 */
+=======
+//Function to post entries after validation to table created
+/*let postEntries = () => {
+    alert('Full Name: ' + fNameEntry);
+    return true;
+}*/
+>>>>>>> master
 
